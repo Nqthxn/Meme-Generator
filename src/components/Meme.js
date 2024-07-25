4214264f3d63b6b0a27e736465281bce5636bfd6
Nathan Lam <nathanl1605@gmail.com>
2024-07-25 13:57:36 +0100

    Stored 100 random memes data and added functinality to generate randomimage urls

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,19 @@
+import memesData from '../memesData';
+
 export default function Meme(){
+    function getMemeImage(){
+        const len = memesData.data.memes.length;
+        const random = Math.floor(Math.random() * len);
+        console.log(memesData.data.memes[random].url);
+    }
+
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form-input" placeholder="Top text"/>
                 <input type="text" className="form-input" placeholder="Bottom text"/>
-                <button className="form-button">Generate Meme 🖼️</button>
-            </form>
+                <button className="form-button" onClick={getMemeImage}>Generate Meme 🖼️</button>
+            </div>
         </main>
     )
 }
