679f48cfb8a8ac301d7d9ff980ef4429f23dd4b9
Nathan Lam <nathanl1605@gmail.com>
2024-07-27 10:13:03 +0100

    Random img displays when user clicks generate meme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from './components/Header';
 import Meme from './components/Meme';
+import React from 'react';
 
 export default function App(){
+
     return(
         <div className="container">
             <Header />
             <Meme />
+
         </div>
     )
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,19 +1,32 @@
 import memesData from '../memesData';
+import React from 'react';
 
 export default function Meme(){
-    function getMemeImage(){
-        const len = memesData.data.memes.length;
-        const random = Math.floor(Math.random() * len);
-        console.log(memesData.data.memes[random].url);
-    }
 
+    const [memeImage, setMemeImage] = React.useState();
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes;
+        const random = Math.floor(Math.random() * memesArray.length);
+        const imgURL = memesArray[random].url;
+
+        setMemeImage(imgURL);
+    }
     return(
         <main>
             <div className="form">
-                <input type="text" className="form-input" placeholder="Top text"/>
-                <input type="text" className="form-input" placeholder="Bottom text"/>
+                <div className="input-container">
+                    <input type="text" className="form-input first-input" placeholder="Top text"/>
+                    <input type="text" className="form-input" placeholder="Bottom text"/>
+                </div>
                 <button className="form-button" onClick={getMemeImage}>Generate Meme 🖼️</button>
+            </div>
+            <div className="image-container">
+                <img src={memeImage} />
             </div>
         </main>
     )
 }
+
+
+
